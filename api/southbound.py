e8e902804dfdd62f26acb7649e726efcd49584e3
Ben Smyth <bensmyth@Bens-MacBook-Pro.local>
2022-05-10 12:17:35 +0100

    ben git ignore

--- api/southbound.py
+++ api/southbound.py
@@ -1,1 +1,8 @@
-# to be continued
+
+# eventually move all functions from upgrader-cli into here.
+
+def auth_test():
+    """
+    Test that the configured auth method is setup correctly, using the specific test sequence for the auth method
+    """
+    pass
